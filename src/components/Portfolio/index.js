b77b4173cd8767b2bbdf685e05a58b642fc1a096
Juanita\Lukas Roberson <lukeroberson24@yahoo.com>
2021-10-07 11:59:18 -0400

    updated portfolio and added images

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -4,17 +4,59 @@ import Projects from '../Projects'
 function Portfolio() {
     const [projects] = useState([
         {
-            name: 'project1',
-            description: 'words',
-            link: 'more words',
-            repo: "Github link"
+            name: 'Plan Rain Check',
+            description: 'words x2',
+            link: 'https://kevinlacarrubba.github.io/plan-rain-check/',
+            repoLink: 'https://github.com/KevinLaCarrubba/plan-rain-check'
         },
         {
-            name: 'project2',
+            name: 'Crypto Forum',
+            description: 'words x2',
+            link: 'https://cryptotalk-forum.herokuapp.com/',
+            repoLink: 'https://github.com/KevinLaCarrubba/Crypto-Forum'
+        },
+        {
+            name: 'Project 3',
             description: 'words x2',
             link: 'more words x2',
-            repo: 'github link x2'
-        }
+            repoLink: 'github link x2'
+        },
+        {
+            name: 'Code Refractor',
+            description: 'I was given the task of refractoring this mock website during my bootcamp in coding.',
+            link: 'https://thelokiszn.github.io/codeRefractor/',
+            repoLink: 'https://github.com/TheLokiSZN/codeRefractor'
+        },
+        {
+            name: 'Password Generator',
+            description: 'words x2',
+            link: 'https://thelokiszn.github.io/PW-Repo/',
+            repoLink: 'https://github.com/TheLokiSZN/PW-Repo'
+        },
+        {
+            name: 'Quiz Game',
+            description: 'words x2',
+            link: 'https://thelokiszn.github.io/Quiz/',
+            repoLink: 'https://github.com/TheLokiSZN/Quiz'
+        },
+        {
+            name: 'Day Planner',
+            description: 'words x2',
+            link: 'https://thelokiszn.github.io/Day-Planner/',
+            repoLink: 'https://github.com/TheLokiSZN/Day-Planner'
+        },
+        {
+            name: 'MVC Tech Blog',
+            description: 'words x2',
+            link: 'https://mvc-tech-blog-2.herokuapp.com/',
+            repoLink: 'https://github.com/TheLokiSZN/MVC-Tech-blog2'
+        },
+        {
+            name: 'Workout Tracker',
+            description: 'words x2',
+            link: 'https://workout-tracker424.herokuapp.com/',
+            repoLink: 'https://github.com/TheLokiSZN/Workout-Tracker'
+        },
     ])
     return (
         <div>
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Projects({ projects }) {
-  const { name, link, description, repo } = projects;
+  const { name, link, description, repoLink } = projects;
   return (
     <div>
       <div className="projects" key={name}>
@@ -12,10 +12,13 @@ function Projects({ projects }) {
         ></img>
         <div className="projectText">
           <h3>
-            <a href={link}>{name}</a>
+            <a href={link}>Project: {name}</a>
           </h3>
           <p>
-            {description} {repo}
+            {description}
+          </p>
+          <p>
+            <a href={repoLink}>Repo: {name}</a>
           </p>
         </div>
       </div>
